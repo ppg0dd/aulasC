586ca51a2796c244f3a7f300b158e09ac1af8524
ppg0dd <126162932+ppg0dd@users.noreply.github.com>
2023-08-11 08:40:05 -0300

    if e else

--- 11-08-23.c
+++ 11-08-23.c
@@ -31,3 +31,50 @@ int main()
         }
     }
 }
+
+//outra forma de executar o mesmo objetivo
+
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, num3, menor, medio, maior;
+    
+    printf("Digite o numero 1: ");
+    scanf("%d", &num1);
+    printf("Digite o numero 2: ");
+    scanf("%d", &num2);
+    printf("Digite o numero 3: ");
+    scanf("%d", &num3);
+    
+    if ((num1 > num2) && (num1 > num3)) {
+        maior = num1;
+        if(num2 > num3) {
+            medio = num2;
+            menor = num3;
+        } else {
+            medio = num3;
+            menor = num2;
+        }
+    } else if ((num2 > num1) && (num2 > num3)) {
+        maior = num2;
+        if(num1 > num3) {
+            medio = num1;
+            menor = num3;
+        } else {
+            medio = num3;
+            menor = num1;
+        }
+    } else {
+        maior = num3;
+        if(num1 > num2) {
+            medio = num1;
+            menor = num2;
+        } else {
+            medio = num2;
+            menor = num1;
+        }
+    }
+    
+    printf("\nO maior número é o %d, o número do meio é o %d, e o menor número é o %d", maior, medio, menor);
+}
