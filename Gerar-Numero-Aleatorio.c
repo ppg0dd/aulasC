272302ae0be3fe67e13760a76a87305bc88d79a8
ppg0dd <126162932+ppg0dd@users.noreply.github.com>
2023-08-16 13:58:50 -0300

    Create Gerar-Numero-Aleatorio.c

---
+++ Gerar-Numero-Aleatorio.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> //precisa dessa biblioteca para funcionar
+
+int main()
+{
+    //comando para gerar um número novo toda vez que rodar
+    int segundos = time(0);
+    srand(segundos);
+    
+    
+    int a = rand(); //número aleatório que ocupa todos os espaços do int
+    int b = rand() % 100; //número aleatório entre 0 e 99
+    int c = (rand() % 100) + 1; //número aleatório entre 1 e 100
+    int d = (rand() % 10) + 1; //número aleatório entre 1 e 10
+    int e = ((rand() % 10) + 1) / 2; //número aleatório entre 1 e 5
+    int f = ((rand() % 10) + 1) / 5; //número aleatório entre 1 e 2
+
+    printf("%d %d %d %d %d", a, b, c, d, e, f);
+}
