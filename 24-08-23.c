4f118cd57ec0879bb935500c43e458b7c17dc895
ppg0dd <126162932+ppg0dd@users.noreply.github.com>
2023-08-24 10:28:53 -0300

    IMC

--- 24-08-23.c
+++ 24-08-23.c
@@ -1,41 +1,40 @@
-//cálculo IMC
 
 #include <stdio.h>
 
 int main(void)
 {
     float H, M, IMC;
-    printf("Digite o peso (kg): ");
+    printf("\nDigite o peso (kg): ");
     scanf("%f", &M);
     if(M <= 0) {
-        printf("Peso inválido");
+        printf("Peso inválido\n");
         return 0;
     }
     printf("Digite a altura (m): ");
     scanf("%f", &H);
     if(H <= 0) {
-        printf("Altura inválida");
+        printf("Altura inválida\n");
         return 0;
     }
     IMC = M / (H*H);
     printf("IMC: %.2f\n", IMC);
     
     if (IMC < 16) {
-        printf("Magreza grau III");
+        printf("Magreza grau III\n");
     } else if(IMC < 17.0) {
-        printf("Magreza grau II");
+        printf("Magreza grau II\n");
     } else if(IMC < 18.5) {
-        printf("Magreza grau I");
+        printf("Magreza grau I\n");
     } else if(IMC < 25.0) {
-        printf("Peso adequado");
+        printf("Peso adequado\n");
     } else if(IMC < 30.0) {
-        printf("Pré-obeso");
+        printf("Pré-obeso\n");
     } else if(IMC < 35.0) {
-        printf("Obesidade grau I");
+        printf("Obesidade grau I\n");
     } else if(IMC < 40.0) {
-        printf("Obesidade grau II");
+        printf("Obesidade grau II\n");
     } else {
-        printf("Obesidade grau III");
+        printf("Obesidade grau III\n");
     }
     
     return 0;
