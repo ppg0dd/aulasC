bd05c1cbb8d68f245e98166b04be3862381e874a
ppg0dd <126162932+ppg0dd@users.noreply.github.com>
2023-10-19 10:32:09 -0300

    Add files via upload

---
+++ triangDePascal.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+
+//O único problema desse código é que no começo do triângulo ele imprime  1 1, sendo que era para imprimir apenas 1;
+
+int fatorial (int x);
+int combinacao (int n, int p);
+
+int main (void) {
+	int linhas;
+	printf("Digite quantas linhas voce quer no triangulo de pascal: ");
+	scanf("%d", &linhas);
+	int i, j;
+	for(i = 0; i <= linhas; i++) {
+		for(j = 0; j <= linhas; j++) {
+			if (combinacao(i, j) != 0) {
+				printf("%d ", combinacao(i, j));
+			}
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
+
+int fatorial (int x) {
+	int resultado = 1;
+	int i;
+	for(i = 2; i <= x; i++) {
+		resultado *= i;
+	}
+	return resultado;
+}
+
+int combinacao (int n, int p) {
+	int resultado;
+	int a = fatorial(n);
+	int b = fatorial(p) * fatorial (n - p);
+	resultado = a / b;
+	return resultado;
+}
